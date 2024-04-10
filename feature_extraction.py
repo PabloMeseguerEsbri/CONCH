2ebf6d88bf2332f3b41cc511da39ff34b27e5970
Pablo Meseguer Esbri <pabmees@upv.es>
2024-04-10 12:21:01 +0200

    solving random and reverse

--- feature_extraction.py
+++ feature_extraction.py
@@ -38,7 +38,7 @@ elif args.data_loading == "AI4SKIN":
     list_wsi = pd.read_csv("./docs/csv/AI4SKIN_WSI.csv", delimiter=",")['WSI'].values
     patches_ids = np.array([patch[:9] for patch in patches])
 
-list_wsi.reverse() if args.reverse else None
+list_wsi = list_wsi[::-1] if args.reverse and args.data_loading == "AI4SKIN" else list_wsi.reverse() if args.reverse else list_wsi
 random.shuffle(list_wsi) if args.random else None
 
 for name_wsi in list_wsi:
