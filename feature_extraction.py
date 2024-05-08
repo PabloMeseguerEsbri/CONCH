b17a09664a5835fb6fd43a13c283b7235f8df3ce
Pablo Meseguer Esbri <pabmees@upv.es>
2024-05-08 15:57:16 +0200

    commit slicing

--- feature_extraction.py
+++ feature_extraction.py
@@ -10,6 +10,28 @@ from PIL import Image
 from tqdm import tqdm
 from conch.open_clip_custom import create_model_from_pretrained
 
+def slicing(images, coords, size_out):
+    size_in = images.shape[1]
+    resize_factor = int(size_in / size_out)
+    steps = [i * (size_in // resize_factor) for i in range(resize_factor)]
+    new_patches, new_coords = [], []
+    for it, img_patch in enumerate(tqdm(images)):
+        coords_patch = coords[it]
+        for x_step in steps:
+            for y_step in steps:
+                resized_patch = img_patch[x_step:x_step + size_out, y_step:y_step + size_out,:]
+                if isWhitePatch_S(resized_patch, rgbThresh=220, percentage=0.5):
+                    continue
+                if isBlackPatch_S(resized_patch, rgbThresh=20, percentage=0.05):
+                    continue
+                resized_coords = [coords_patch[0]+x_step, coords_patch[1]+y_step]
+                new_patches.append(resized_patch)
+                new_coords.append(resized_coords)
+
+    new_patches = np.stack(new_patches)
+    new_coords = np.stack(new_coords)
+    return new_patches, new_coords
+
 parser = argparse.ArgumentParser(description="Feature extraction w/ CONCH")
 parser.add_argument('--folder_path', type=str)
 parser.add_argument('--folder_save', type=str)
