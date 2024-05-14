23006ee62d98b548a8e8390c85b0308fe9b2cbe5
Pablo Meseguer Esbri <pabmees@upv.es>
2024-05-14 10:43:41 +0200

    Update MIZero for pulmonary

--- zero_shot.py
+++ zero_shot.py
@@ -15,33 +15,51 @@ parser.add_argument('--folder', type=str)
 parser.add_argument('--project', type=str, default = "RCC")
 args = parser.parse_args()
 
-# Data loading
-data = pd.read_csv("local_data/csv/" + args.project + ".csv")
-list_WSI = data["WSI"]
-labels = data["GT"]
-features = [np.load(os.path.join(args.folder, fn + ".npy")) for fn in tqdm(list_WSI)]
-
+# Data preparation
 if args.project == "RCC":
     idx_to_class = {0: "CHRCC", 1: "CCRCC", 2: "PRCC"}
+    classes = ["Renal cell carcinoma, chromophobe type",
+               "Clear cell adenocarcinoma, NOS",
+               "Papillary adenocarcinoma, NOS"]
+if args.project == "AI4SKIN":
+    idx_to_class = {0: "lm", 1: "lms", 2: "df", 3: "dfs", 4: "mel", 5:"fxa"}
+    classes = ["Leiomyoma",
+               "Leiomyosarcoma",
+               "Dermatofibroma",
+               "Dermatofibrosarcoma",
+               "Melanoma",
+               "Fibroxhantoma"]
+if args.project == "NSCLC":
+    idx_to_class = {0: "LUAD", 1: "LUSC"}
+    classes = ["Adenocarcinoma, NOS",
+               "Squamous cell carcinoma, NOS"]
+
+data = pd.read_csv("local_data/csv/" + args.project + ".csv")
+features = [np.load(os.path.join(args.folder, fn + ".npy")) for fn in tqdm(data["WSI"])]
+labels = np.array([classes.index(item) for item in data["GT"]], dtype=np.int64)
 
 # Model building
 device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
-checkpoint_path = './checkpoints/CONCH/pytorch_model.bin'
+checkpoint_path = '../feature_extraction/local_data/model/pytorch_model.bin'
 model, _ = create_model_from_pretrained(model_cfg='conch_ViT-B-16', checkpoint_path=checkpoint_path, device=device)
 _ = model.eval()
 
 # Prompt ensembling
 BACC_mean, BACC_top1, BACC_top5, BACC_top10, BACC_top50, BACC_top100, BACC_top500, BACC_top1000 = [], [], [], [], [], [], [], []
-prompt_file = './prompts/rcc_multiprompt.json'
-for it in tqdm(range(50)):
-    with open(prompt_file) as f:
-        prompts = json.load(f)[str(it)]
+prompt_file = './prompts/' + args.project + '.json'
+with open(prompt_file) as f:
+    all_prompts = json.load(f)
+
+for it in tqdm(range(len(all_prompts))):
+    prompts = all_prompts[str(it)]
     classnames = prompts['classnames']
     templates = prompts['templates']
     n_classes = len(classnames)
     classnames_text = [classnames[str(idx_to_class[idx])] for idx in range(n_classes)]
     zeroshot_weights = zero_shot_classifier(model, classnames_text, templates, device=device)
 
+    # zeroshot_weights = np.load("../MILAdapters/local_data/prompts/NSCLC_promptensemble.npy")
+    # zeroshot_weights = torch.tensor(zeroshot_weights).T.cuda()
     preds_top1, preds_top5, preds_top10, preds_top50, preds_top100, preds_top500, preds_top1000, preds_mean = [], [], [], [], [], [], [], []
     for image_features in features:
         image_features = torch.tensor(image_features).cuda()
